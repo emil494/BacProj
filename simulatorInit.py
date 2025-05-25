043b33ed45f38b6a3a71c83ddffc68b80ad28817
simonsionsom <simon19653@gmail.com>
2025-05-25 13:10:37 +0200

    jeg fik den til at due, nu burde det virker yuuuhuuu, den kan finde ud af nu om der er flere relationer en blot en per event

--- simulatorInit.py
+++ simulatorInit.py
@@ -78,33 +78,33 @@ def createDCRgraph(graph):
                 if isinstance(targets, list):
                     for target in targets:
                         if cond == 'response':
-                            if dcr.responses[target['@sourceId']] in dcr.responses:
+                            if target['@sourceId'] in dcr.responses:
                                 dcr.responses[target['@sourceId']]+=[target['@targetId']]
                             else:
                                 dcr.responses[target['@sourceId']]=[target['@targetId']]
                         if cond == 'exclude':
                             print(targets)
-                            if dcr.excludes[target['@sourceId']] in dcr.excludes:
+                            if target['@sourceId'] in dcr.excludes:
                                 dcr.excludes[target['@sourceId']]+=[target['@targetId']]
                             else:
                                 dcr.excludes[target['@sourceId']]=[target['@targetId']]
                             
                         if cond == 'include':
-                            if dcr.includes[target['@sourceId']] in dcr.includes:
+                            if target['@sourceId'] in dcr.includes:
                                 dcr.includes[target['@sourceId']]+=[target['@targetId']]
                             else:
                                 dcr.includes[target['@sourceId']]=[target['@targetId']]
                             
                         if cond == 'milestone':
                             if '@link' not in target.keys():
-                                if dcr.milestones[target['@sourceId']] in dcr.milestones:
+                                if target['@sourceId'] in dcr.milestones:
                                     dcr.milestones[target['@sourceId']]+=[target['@targetId']]
                                 else:
                                     dcr.milestones[target['@sourceId']]=[target['@targetId']]
                         if cond == 'condition':
                             print(target['@targetId'])
                             print(target['@sourceId'])
-                            if dcr.conditions[target['@targetId']] in dcr.conditions:
+                            if target['@targetId'] in dcr.conditions:
                                 dcr.conditions[target['@targetId']]+=[target['@sourceId']]
                             else:
                                 dcr.conditions[target['@targetId']]=[target['@sourceId']]
